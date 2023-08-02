81c146220fdd93eb6b30f1c5bbc567e21d5941a5
Eoin Treacy <eointreacy5@gmail.com>
2023-08-02 22:18:47 +0100

    created button builder, in preparation for modular array shuffler for easy quiz mode

--- src/Tile.jsx
+++ src/Tile.jsx
@@ -1,6 +1,21 @@
 import VocabButton from "./VocabButton.jsx"
 import {useState} from "react"
 
+function ButtonBuilder ({fr, es, pt, it}) {
+  let dialects = [
+    { vocab: fr, flag: "fr" },
+    { vocab: es, flag: "es" },
+    { vocab: pt, flag: "pt" },
+    { vocab: it, flag: "it" }
+  ]
+  
+  let buttons = dialects.map((d) => (
+      <VocabButton lang={d.vocab} flag={d.flag}/>
+  ))
+
+  return(buttons)
+}
+
 export default function Tile({ en, src, fr, es, pt, it }) {
     const [quiz, setQuiz] = useState(false)
     
@@ -10,15 +25,11 @@ export default function Tile({ en, src, fr, es, pt, it }) {
 
     return (
       <>
-      
+
       <div>
         <img className="tilePic" src={src} alt={en} />
-        <VocabButton lang={fr} flag="fr"/>
-        <VocabButton lang={es} flag="es"/>
-        <VocabButton lang={pt} flag="pt"/>
-        <VocabButton lang={it} flag="it"/>
+        <ButtonBuilder fr={fr} es={es} pt={pt} it={it}/>
       </div>
-
 
       <div>
         <button onClick={handleClick}>{quiz ? "Do Over" : "Quiz"}</button>
