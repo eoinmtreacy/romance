09f4c69349689dc40a0ed9a16bb2dd9d749732c4
Eoin Treacy <eointreacy5@gmail.com>
2023-08-02 19:18:08 +0100

    added flags to each language button

--- src/Tile.jsx
+++ src/Tile.jsx
@@ -1,12 +1,21 @@
 export default function Tile({ en, src, fr, es, pt, it }) {
     return (
+      <>
       <div>
         <img className="tilePic" src={src} alt={en} />
-        <button>{fr}</button>
-        <button>{es}</button>
+        <img alt="fr" src="/fr.svg"></img>
+          <button>{fr}</button>
+        <img alt="es" src="/es.svg">
+          </img><button>{es}</button>
+          <img alt="pt" src="/pt.svg"></img>
         <button>{pt}</button>
+          <img alt="it" src="/it.svg"></img>
         <button>{it}</button>
       </div>
+      <div>
+        <button>Quiz</button>
+      </div>
+      </>
     );
   }
   
