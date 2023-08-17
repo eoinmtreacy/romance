6e82d4c5f73901a18abf49ed4b1ff572e9f3ac86
Eoin Treacy <eointreacy5@gmail.com>
2023-08-17 17:42:20 +0100

    guessing behaviour now confined to own component

--- src/Tile.jsx
+++ src/Tile.jsx
@@ -1,7 +1,7 @@
 import VocabButton from "./VocabButton.jsx"
 import {shuffler} from "./shuffler.js"
 
-function ButtonBuilder ({fr, es, pt, it, quiz, guess, setGuess}) {
+function ButtonBuilder ({fr, es, pt, it, quiz}) {
 
 
   let dialects = [
@@ -11,30 +11,30 @@ function ButtonBuilder ({fr, es, pt, it, quiz, guess, setGuess}) {
     { vocab: it, flag: "it"}
   ]
 
-  if (quiz === true && guess === "") {
-    shuffler(dialects)
+  if (quiz === true) {
+    dialects = shuffler(dialects)
   } 
 
   let buttons = dialects.map((d) => (
-      <VocabButton lang={d.vocab} flag={d.flag} isTarget={false} guess={guess} setGuess={setGuess}/>
+      <VocabButton lang={d.vocab} flag={d.flag} isTarget={false} quiz={quiz}/>
   ))
 
   if (quiz === true) {
     buttons.splice(0,1,
-      <VocabButton lang="" flag={dialects[0].flag} isTarget={true} guess={guess} setGuess={setGuess}/>)
+      <VocabButton lang="" flag={dialects[0].flag} isTarget={true} quiz={quiz}/>)
   }
 
   
   return(buttons)
 }
 
-export default function Tile({ en, src, fr, es, pt, it, quiz, handleQuizClick, guess, setGuess}) {
+export default function Tile({ en, src, fr, es, pt, it, quiz, handleQuizClick}) {
 
     return (
       <>
       <div>
         <img className="tilePic" src={src} alt={en} />
-        <ButtonBuilder fr={fr} es={es} pt={pt} it={it} quiz={quiz} guess={guess} setGuess={setGuess}/>
+        <ButtonBuilder fr={fr} es={es} pt={pt} it={it} quiz={quiz}/>
       </div>
 
       <div>
--- src/VocabButton.jsx
+++ src/VocabButton.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react"
 
-export default function VocabButton ({lang, flag, isTarget}) {
+export default function VocabButton ({lang, flag, isTarget, quiz}) {
   const [guess, setGuess] = useState("")
+
+  if (quiz === false && guess !== "") {
+    setGuess("")
+  }
 
     return(
       <>
       <img alt={lang} src={"/" + flag + ".svg"}></img>
           
-          <input className="vocabBox" type="text" value={isTarget ? guess : lang} onChange={isTarget ? (e) => setGuess(e.target.value) : null}></input>
+          <input className="vocabBox" type="text" value={isTarget ? guess : lang} onChange={isTarget ? (e) => setGuess(e.target.value) : (e) => setGuess(lang)}></input>
           </>
     )
   }
