6de5c2b80799c2143d75455d3ab0ba9d15e4c8e8
Eoin Treacy <eointreacy5@gmail.com>
2023-08-17 18:18:55 +0100

    levenshtein added, prepped for guess checking

--- src/VocabButton.jsx
+++ src/VocabButton.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { lvs } from "./lvs"
 
 export default function VocabButton ({lang, flag, isTarget, quiz}) {
   const [guess, setGuess] = useState("")
@@ -12,6 +13,9 @@ export default function VocabButton ({lang, flag, isTarget, quiz}) {
       <img alt={lang} src={"/" + flag + ".svg"}></img>
           
           <input className="vocabBox" type="text" value={isTarget ? guess : lang} onChange={isTarget ? (e) => setGuess(e.target.value) : (e) => setGuess(lang)}></input>
+
+          <b>      </b>
+
           </>
     )
   }
