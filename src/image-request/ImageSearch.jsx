43c4006f672e58bcc0e206587bf487fc07ac23c8
Eoin Treacy <eointreacy5@gmail.com>
2023-08-23 14:33:41 +0100

    pre-request next 'current' image URL to reduce the lag on clicking next

--- src/image-request/ImageSearch.jsx
+++ src/image-request/ImageSearch.jsx
@@ -2,35 +2,60 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const ImageSearch = ({current, blob}) => {
-  const [imageUrl, setImageUrl] = useState('');
-  const [urlArray, setUrlArray] = useState([])
+  const [urlArr, setUrlArr] = useState([])
 
-  const prompt = blob[current].en
+  const apiKey = 'qSzMCedWrSstW0PhIwfvY72moOSWQfMgWPT9bxHTcxkyCuUiZM4rTA1i';
 
-  const handleSearch = async () => {
-    const apiKey = 'qSzMCedWrSstW0PhIwfvY72moOSWQfMgWPT9bxHTcxkyCuUiZM4rTA1i';
-    const apiUrl = `https://api.pexels.com/v1/search?query=${prompt}&per_page=1`;
+  let urls = [];
 
-    try {
-        const response = await axios.get(apiUrl, {
-          headers: {
-            Authorization: apiKey,
-          },
-        });
-  
-        const imageLink = response.data.photos[0].src.large;
-        setImageUrl(imageLink);
-      } catch (error) {
-        console.error('Error fetching image:', error);
+  for (let i = 0; i < 2; i++) {
+    let url =
+      "https://api.pexels.com/v1/search?query=" + blob[i].en + "&per_page=1";
+    urls.push(url);
+  }
+
+  if (urlArr.length === current) {
+    const requests = urls.map((url) =>
+      axios.get(url, {
+        headers: {
+          Authorization: apiKey
+        }
+      })
+    );
+
+    let reqArr = [];
+
+    axios.all(requests).then((responses) => {
+      responses.forEach((resp) => {
+        reqArr.push(resp.data.photos[0].src.large);
+      }, setUrlArr(reqArr));
+    });
+  }
+
+  if (current + 2 > urlArr.length) {
+    let url =
+      "https://api.pexels.com/v1/search?query=" +
+      blob[current + 1].en +
+      "&per_page=1";
+    const request = axios.get(url, {
+      headers: {
+        Authorization: apiKey
       }
-    };
+    });
+    request.then((resp) => {
+      let reqArr = [...urlArr];
+      reqArr.push(resp.data.photos[0].src.large);
+      setUrlArr(reqArr);
+      console.log(reqArr);
+    });
+  }
 
-  handleSearch();
+  //setImageUrl(urlArr[current])
 
   return (
     <div>
-      <h3>Plexel</h3>
-      {imageUrl && <img className="logo" src={imageUrl} alt="Searched" />}
+      <h3>Source: Pexel</h3>
+      {<img className="logo" src={urlArr[current]} alt="Searched" />}
     </div>
   );
 };
