2f165399af3f3b789015dd97d8eea75c7b6f8135
Eoin Treacy <eointreacy5@gmail.com>
2023-08-30 11:02:41 +0100

    adjusted prefetch of images to fix most of the lag

--- src/image-request/ImageSearch.jsx
+++ src/image-request/ImageSearch.jsx
@@ -1,69 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const ImageSearch = ({current, blob}) => {
-  const [urlArr, setUrlArr] = useState([])
-
+const ImageSearch = ({ current, blob }) => {
+  const [urlArr, setUrlArr] = useState([]);
   const apiKey = 'qSzMCedWrSstW0PhIwfvY72moOSWQfMgWPT9bxHTcxkyCuUiZM4rTA1i';
 
-  let urls = [];
+  useEffect(() => {
+    const fetchImages = async () => {
+      const newUrlArr = [...urlArr];
 
-  for (let i = 0; i < 2; i++) {
-    let url =
-      "https://api.pexels.com/v1/search?query=" + blob[i].en + "&per_page=1";
-    urls.push(url);
-  }
-
-  if (urlArr.length === current) {
-    const requests = urls.map((url) =>
-      axios.get(url, {
-        headers: {
-          Authorization: apiKey
+      for (let i = 0; i < 3; i++) {
+        try {
+          const response = await axios.get(
+            `https://api.pexels.com/v1/search?query=${blob[current + i].en}&per_page=1`,
+            {
+              headers: {
+                Authorization: apiKey,
+              },
+            }
+          );
+          newUrlArr[current + i] = response.data.photos[0].src.large;
+        } catch (error) {
+          console.error("Error fetching image:", error);
         }
-      })
-    );
-
-    let reqArr = [];
-
-    axios.all(requests).then((responses) => {
-      responses.forEach((resp) => {
-        reqArr.push(resp.data.photos[0].src.large);
-      }, setUrlArr(reqArr));
-    });
-  }
-
-  if (current + 2 > urlArr.length) {
-    let url =
-      "https://api.pexels.com/v1/search?query=" +
-      blob[current + 1].en +
-      "&per_page=1";
-    const request = axios.get(url, {
-      headers: {
-        Authorization: apiKey
       }
-    });
-    request.then((resp) => {
-      let reqArr = [...urlArr];
-      reqArr.push(resp.data.photos[0].src.large);
-      setUrlArr(reqArr);
-      console.log(reqArr);
-    });
-  }
+
+      setUrlArr(newUrlArr);
+    };
+
+    if (blob.length > current + 2 && !urlArr[current + 2]) {
+      fetchImages();
+    }
+  }, [current, blob, urlArr]);
+
 
   return (
-<div className="font-sans grid grid-cols-1 content-center">
-  <div className="content-center">
-    <h3 className="mb-2 text-gray-600">Source: Pexel</h3>
-    <div className="w-[25vw] h-[25vw] mb-2 relative">
-      <img
-        src={urlArr[current]}
-        alt="Searched"
-        className="w-full h-full rounded-lg object-cover object-center"
-      />
+    <div className="font-sans grid grid-cols-1 content-center">
+      <div className="content-center">
+        <h3 className="mb-2 text-gray-600">Source: Pexel</h3>
+        {urlArr[current] && ( // Display image when it's available
+          <div className="w-[25vw] h-[25vw] mb-2 relative">
+            <img
+              src={urlArr[current]}
+              alt="Searched"
+              className="w-full h-full rounded-lg object-cover object-center"
+            />
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-</div>
-
   );
 };
 
